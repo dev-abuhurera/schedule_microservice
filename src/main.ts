50c4268d469b57dcc0bd879620eac1170bdaf67f
dev-abuhurera <abuhurerarohani@gmail.com>
2026-02-01 19:11:51 +0500

    api documntation through swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder().setTitle('Schedular Microservice').setDescription('Api for job scheduling').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
 
   app.useGlobalPipes(new ValidationPipe({
 
@@ -13,6 +17,8 @@ async function bootstrap() {
     transform: true,
 
   }));
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
   console.log('Server running on http://localhost:3000');
